a72b36c40c8a177fa79ee7a16f03af0e57225890
yingzhuo <yingzhor@gmail.com>
2020-08-15 10:22:42 +0800

    2020-08-15 10:22:42

---
+++ .shell/pull-1.18.7.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+for IT in coredns:1.6.7 etcd:3.4.3-0 pause:3.2 kube-proxy:v1.18.7 kube-scheduler:v1.18.7 kube-controller-manager:v1.18.7 kube-apiserver:v1.18.7
+do
+    docker image pull "registry.cn-shanghai.aliyuncs.com/yingzhuo/$IT"
+    docker image tag  "registry.cn-shanghai.aliyuncs.com/yingzhuo/$IT" "k8s.gcr.io/$IT"
+    docker image rmi  "registry.cn-shanghai.aliyuncs.com/yingzhuo/$IT"
+done
