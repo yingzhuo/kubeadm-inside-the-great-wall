57ea5f15af1c72a01fe383fbaf042d0182798c4a
yingzhuo <yingzhor@gmail.com>
2020-10-20 05:29:04 +0800

    2020-10-20 05:29:04

---
+++ .shell/pull-1.16.15.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+for IT in coredns:1.6.2 etcd:3.3.15-0 pause:3.1 kube-proxy:v1.16.15 kube-scheduler:v1.16.15 kube-controller-manager:v1.16.15 kube-apiserver:v1.16.15
+do
+    docker image pull "registry.cn-shanghai.aliyuncs.com/yingzhuo/$IT"
+    docker image tag  "registry.cn-shanghai.aliyuncs.com/yingzhuo/$IT" "k8s.gcr.io/$IT"
+    docker image rmi  "registry.cn-shanghai.aliyuncs.com/yingzhuo/$IT"
+done
