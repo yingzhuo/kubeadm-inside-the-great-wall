69f138381b0a2dacb9dcdcc816eab9ff25ae0bf4
yingzhuo <yingzhor@gmail.com>
2020-10-22 05:30:08 +0800

    2020-10-22 05:30:08

---
+++ .shell/pull-1.15.9.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+for IT in coredns:1.3.1 etcd:3.3.10 pause:3.1 kube-proxy:v1.15.9 kube-scheduler:v1.15.9 kube-controller-manager:v1.15.9 kube-apiserver:v1.15.9
+do
+    docker pull "registry.cn-shanghai.aliyuncs.com/yingzhuo/$IT"
+    docker tag  "registry.cn-shanghai.aliyuncs.com/yingzhuo/$IT" "k8s.gcr.io/$IT"
+    docker rmi  "registry.cn-shanghai.aliyuncs.com/yingzhuo/$IT"
+done
