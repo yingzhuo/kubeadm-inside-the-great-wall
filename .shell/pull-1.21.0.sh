ca1fef6a0336f148626f310ef9a9fa570a1fffaa
应卓 <yingzhor@gmail.com>
2021-04-21 09:46:39 +0800

    2021-04-21 09:46:39

--- .shell/pull-1.21.0.sh
+++ .shell/pull-1.21.0.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-for IT in coredns/coredns:v1.8.0 etcd:3.4.13-0 pause:3.4.1 kube-proxy:v1.21.0 kube-scheduler:v1.21.0 kube-controller-manager:v1.21.0 kube-apiserver:v1.21.0
+docker image pull "registry.cn-shanghai.aliyuncs.com/yingzhuo/coredns:v1.8.0"
+docker image tag  "registry.cn-shanghai.aliyuncs.com/yingzhuo/coredns:v1.8.0" "k8s.gcr.io/coredns/coredns:v1.8.0"
+docker image rmi  "registry.cn-shanghai.aliyuncs.com/yingzhuo/coredns:v1.8.0"
+
+for IT in etcd:3.4.13-0 pause:3.4.1 kube-proxy:v1.21.0 kube-scheduler:v1.21.0 kube-controller-manager:v1.21.0 kube-apiserver:v1.21.0
 do
     docker image pull "registry.cn-shanghai.aliyuncs.com/yingzhuo/$IT"
     docker image tag  "registry.cn-shanghai.aliyuncs.com/yingzhuo/$IT" "k8s.gcr.io/$IT"
