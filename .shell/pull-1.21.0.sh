bfe89f25f64ffca0bd1d6022663e071953127373
应卓 <yingzhor@gmail.com>
2021-04-21 09:29:18 +0800

    2021-04-21 09:29:18

---
+++ .shell/pull-1.21.0.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+for IT in coredns:v1.8.0 etcd:3.4.13-0 pause:3.4.1 kube-proxy:v1.21.0 kube-scheduler:v1.21.0 kube-controller-manager:v1.21.0 kube-apiserver:v1.21.0
+do
+    docker image pull "registry.cn-shanghai.aliyuncs.com/yingzhuo/$IT"
+    docker image tag  "registry.cn-shanghai.aliyuncs.com/yingzhuo/$IT" "k8s.gcr.io/$IT"
+    docker image rmi  "registry.cn-shanghai.aliyuncs.com/yingzhuo/$IT"
+done
--- README.md
+++ README.md
@@ -16,7 +16,10 @@ kubeadm init \
 如果你需要直接拉取镜像，则可以使用这些[脚本](./.shell)。
 
 ```bash
-# v1.20.6 (latest)
+# v1.21.0 (latest)
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/yingzhuo/kubeadm-inside-the-great-wall/master/.shell/pull-1.21.0.sh)"
+
+# v1.20.6
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/yingzhuo/kubeadm-inside-the-great-wall/master/.shell/pull-1.20.6.sh)"
 
 # v1.19.10
